4d60618fbab07f829d598f349bf7c600a335f05f
Antonio Gázquez <antonio.gazquez@redegal.com>
2018-07-26 12:40:32 +0200

    Add basic test

---
+++ test/confirm.js
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+import jsdom from 'mocha-jsdom';
+import confirm from '../src/index';
+
+describe('confirm function', function() {
+    jsdom();
+
+    it('renders modal on calling', async () => {
+        confirm();
+
+        let rendered = document.querySelector('div');
+
+        expect(rendered).to.be.ok;
+    });
+});
